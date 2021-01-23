559e4f9084403c4e611e4df620cb3d2443a9c282
ChrisBAshton <chrisashtonweb@gmail.com>
2021-01-23 09:48:19 +0000

    Sanitize inputs

--- functions.php
+++ functions.php
@@ -1,14 +1,19 @@
 <?php
 add_filter('style_loader_tag',  'inline_cacher_logic', 10, 4);
 function inline_cacher_logic( $html, $handle, $href, $media ) {
+  $href = esc_url($href);
   preg_match('/\?ver=(.*)$/', $href, $matches);
   if ($matches[1]) {
-    $version = $matches[1];
+    $version = esc_attr($matches[1]);
     if (InlineCacher::cached($href, $version)) {
       return '<link href="' . $href . '" rel="stylesheet" type="text/css" />';
     }
     else {
-      return '<style data-src="' . $href . '" data-inline-cache="' . $version . '">/*' . file_get_contents($href) . '*/</style>';
+      // we need to retain ', ", & and > in CSS, but there's no need for a <
+      // (which could otherwise open us up to attacks by putting `</style>...`
+      // in the referenced CSS)
+      $inline_css = str_replace("<", "", file_get_contents($href));
+      return '<style data-src="' . $href . '" data-inline-cache="' . $version . '">' . $inline_css . '</style>';
     }
   }
   return $html;
--- inline_cacher.php
+++ inline_cacher.php
@@ -2,6 +2,14 @@
 class InlineCacher {
   public static function cached($url, $hash) {
     $cached = json_decode(stripslashes($_COOKIE["inline-cacher"]));
-    return $cached->$url == $hash;
+    if (!$cached) {
+      // anything that isn't valid JSON will be decoded as null
+      return false;
+    }
+    $sanitized_cache = array();
+    foreach ($cached as $resource_url => $version) {
+      $sanitized_cache[esc_url_raw($resource_url)] = strip_tags($version);
+    }
+    return $sanitized_cache[$url] == $hash;
   }
 }
